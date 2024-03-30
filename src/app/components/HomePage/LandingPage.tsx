818ae12c43b23505da8eb281b48e3b12fa5c86ac
Tuguldur Narandelger <tuuduu00@gmail.com>
2024-03-30 12:31:23 +0800

    homepage

--- src/app/components/HomePage/LandingPage.tsx
+++ src/app/components/HomePage/LandingPage.tsx
@@ -1,15 +1,60 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import CarModel from "./CarModel";
 import Link from "next/link";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 const LandingPage = () => {
+  const ref: any = useRef();
+  const isInView = useInView(ref, { once: false });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
-    <div className="w-full items-center flex-col flex h-screen bg-black ">
-        <p className="font-bold mt-[200px] text-[100px]">Rent the luxury. Own the thrill</p>
-        <div className="h-full  w-full absolute">
+    <div
+      ref={ref}
+      className="w-full items-center flex-col flex h-screen bg-[#0D0D0D] "
+    >
+      <div className="font-bold mt-[200px] relative flex text-[100px]">
+        Rent the
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, y: -75 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 0.5, delay: 2.25 }}
+          className="bg-white text-black"
+        >
+          luxury.
+        </motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 1, y: 0 },
+            visible: { opacity: 0, y: 75 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.5, delay: 2 }}
+          className="bg-white text-black absolute left-[410px]"
+        >
+          basic.
+        </motion.div>
+         Own the <span className="bg-white text-black">thrill</span>.
+      </div>
+      <div className="h-full  w-full absolute">
         <CarModel />
-        </div>
-        <button className="absolute text-xl bottom-[300px] right-[400px] border-2 rounded-xl px-4 py-2 hover:bg-white hover:text-black hover:border-black">Order now</button>
+      </div>
+      <button className="absolute text-xl bottom-[300px] right-[400px] border-2 rounded-xl px-4 py-2 hover:bg-white hover:text-black hover:border-black">
+        Order now
+      </button>
     </div>
   );
 };
--- src/app/components/HomePage/SecondPage.tsx
+++ src/app/components/HomePage/SecondPage.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+import Image from "next/image";
 
 function SecondPage() {
-  return <div className="w-full items-center flex-col flex h-screen bg-[#1A1A1A] "></div>;
+  const ref: any = useRef();
+  const isInView = useInView(ref, { once: false });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div
+      className="w-full flex-col relative flex h-[2500px] gap-10 py-[360px]"
+      style={{ backgroundImage: 'url("/testbg.jpg")', backgroundSize: "cover" }}
+    >
+      <motion.div
+        ref={ref}
+        variants={{
+          hidden: { opacity: 0, y: 25 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-5xl font-bold text-white  w-1/2 ml-[120px]"
+      >
+        Discover the freedom of the open road with DriveAway Car Rentals.
+      </motion.div>
+
+      <p className="text-3xl ml-[120px] w-1/2 flex">
+        Whether you're embarking on a cross-country adventure or simply need
+        reliable transportation for your daily commute, our fleet of top-quality
+        vehicles has you covered.
+      </p>
+    </div>
+  );
 }
 
 export default SecondPage;
