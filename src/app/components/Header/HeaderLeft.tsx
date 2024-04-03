3369d30fb21021dfcc3925d6aedffe52910ce637
Zolushkazz <23lp1625@nest.edu.mn>
2024-04-03 15:34:47 +0800

    asD

--- src/app/components/Header/HeaderLeft.tsx
+++ src/app/components/Header/HeaderLeft.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const HeaderLeft = () => {
   return (
     <div className="flex items-center flex-row justify-center gap-8 relative">
-      <Image src="/ASD2.png" alt="logo" width={80} height={80}/>
+      <Image src="/ASD2.png" alt="logo" width={80} height={80} />
     </div>
   );
 };
