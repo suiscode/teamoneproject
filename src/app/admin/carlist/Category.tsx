9297d4083e1bf1e0fda2fc6bdc99157db44a1582
Tuguldur Narandelger <tuuduu00@gmail.com>
2024-04-05 11:48:48 +0800

    heha

--- src/app/admin/carlist/Category.tsx
+++ src/app/admin/carlist/Category.tsx
@@ -71,7 +71,7 @@ function Category() {
   };
 
   const handleChange = (name: any) => {
-    setSuccess('')
+    setSuccess("");
     const params = new URLSearchParams(searchParams);
     params.set("category", name);
     push(`${pathname}?${params}`);
