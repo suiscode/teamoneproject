65df1d70a5ce7be70d253261cfa5c0dc61216e64
Zolushkazz <23lp1625@nest.edu.mn>
2024-04-09 12:48:57 +0800

    w

--- src/app/cars/favorite/page.tsx
+++ src/app/cars/favorite/page.tsx
@@ -1,4 +1,4 @@
-import CarCart from "@/app/_wcomponents/CardDetail/CarCart";
+import CarCart from "@/app/_components/CardDetail/CarCart";
 import { getAllFavoritedItem } from "@/app/fetch";
 import { CarItem } from "@/lib/interface";
 import React from "react";
@@ -18,9 +18,14 @@ async function page() {
           <CarCart key={item.id} car={item} session={session} />
         ))
       ) : (
-        <h1 className="text-secondary self-center">Please 
-        <Link className="text-blue-400 underline" href='/auth/login'> Sign in </Link>
-          to show bookmarked cars</h1>
+        <h1 className="text-secondary self-center">
+          Please
+          <Link className="text-blue-400 underline" href="/auth/login">
+            {" "}
+            Sign in{" "}
+          </Link>
+          to show bookmarked cars
+        </h1>
       )}
     </div>
   );
