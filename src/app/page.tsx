033c71fb212de346699f4c5df1f32a749e9015e7
Zolushkazz <23lp1625@nest.edu.mn>
2024-04-09 12:44:22 +0800

    zol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,20 @@ import { useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 import React from "react";
+import axios from "axios";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.post("/api/sort/", { name: "zolo" });
+      console.log(res);
+    };
+    fetchData();
+  }, []);
   // gsap.to(window, { duration: 2, scrollTo: 400 });
   return (
     <div className="w-screen flex items-center justify-center flex-col z-10 text-white">
       <HomeHeader />
-
       <LandingPage />
 
       <SecondPage />
