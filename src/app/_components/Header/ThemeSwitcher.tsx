4800821043ff630a902b6e1bd4e719cbcfdbf3e0
Tselmeg Munkhtuul <23LP6227@G302-CC0104.local>
2024-04-09 12:48:07 +0800

    fix

--- src/app/_components/Header/ThemeSwitcher.tsx
+++ src/app/_components/Header/ThemeSwitcher.tsx
@@ -26,7 +26,7 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Light
+          Lights
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Dark
