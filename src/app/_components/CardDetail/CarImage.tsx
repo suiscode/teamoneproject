e8ba84b51e1cde3b0a8425af3f3bbcac5e9ee5ef
Bilegtsaikhan Batsuuri <23LP7255@G302-CC0111.local>
2024-04-15 13:19:37 +0800

    15 hiiv blgt

--- src/app/_components/CardDetail/CarImage.tsx
+++ src/app/_components/CardDetail/CarImage.tsx
@@ -6,8 +6,8 @@ function CarImage({ data }: any) {
   const [state, setState] = useState(0);
 
   return (
-    <div className="flex flex-col w-[48%] h-[500px] gap-5 sm:w-96 md:w-[520px]">
-      <div className="w-full relative h-full">
+    <div className="flex flex-col w-[730px] gap-5 p-8">
+      <div className="w-[665px] h-[350px] relative">
         <Image
           src={data.img[state]}
           alt="image"
@@ -15,34 +15,34 @@ function CarImage({ data }: any) {
           className="rounded-xl absolute"
         />
       </div>
-      <div className="flex justify justify-between">
-        <div className="w-[148px] h-[124px] relative">
+      <div className="flex gap-5">
+        <div className="w-[280px] h-[200px] relative">
           <Image
             src={data.img[0]}
             alt=""
-            onClick={() => setState(0)}
+            onMouseEnter={() => setState(0)}
             fill
-            className="rounded-xl cursor-pointer  absolute"
+            className="rounded-xl cursor-pointer absolute"
           />
         </div>
-        <div className="w-[148px] h-[124px] relative">
+        <div className="w-[280px] h-[200px] relative">
           {data.img[1] && (
             <Image
               src={data.img[1]}
               alt=""
               fill
-              onClick={() => setState(1)}
+              onMouseEnter={() => setState(1)}
               className="rounded-xl  cursor-pointer  absolute"
             />
           )}
         </div>
-        <div className="w-[148px] h-[124px] relative">
+        <div className="w-[280px] h-[200px] relative">
           {data.img[2] && (
             <Image
               src={data.img[2]}
               alt=""
               fill
-              onClick={() => setState(2)}
+              onMouseEnter={() => setState(2)}
               className="rounded-xl cursor-pointer  absolute"
             />
           )}
